85b4b670b10f7ce963d580d20df6e2284c09bc86
Daud1979 <100976611+Daud1979@users.noreply.github.com>
2024-05-16 21:55:43 +0200

    finalizado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 // Tu código aquí
+let btn = document.querySelector("#btn");
+let names= document.querySelector("#first");
+let apellido= document.querySelector("#last");
+btn.addEventListener('click', async ()=>{
+    let response = await fetch('https://randomuser.me/api/');
+    let data = await response.json();
+   // document.querySelector('#name').textContent=data.name;
+   console.log(data); 
+   names.innerHTML=(data.results[0].name.first);
+    apellido.innerHTML=data.results[0].name.last;
+    document.querySelector("#country").innerHTML=data.results[0].location.country;
+    document.querySelector("#phone").innerHTML=data.results[0].phone;
+    document.querySelector("#email").innerHTML=data.results[0].email;
+    document.querySelector("#photo").src=data.results[0].picture.medium;
+});
